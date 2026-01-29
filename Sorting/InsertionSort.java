839ede344d15168404013fabf4bcf26bfb54e290
PooranaSelvan <pooranaselvan67@gmail.com>
2026-01-29 07:53:41 +0530

    Added Bubble & Insertion Sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,29 +1,25 @@
 import java.util.Arrays;
 
-public class BubbleSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {13, 20, 46, 24, 20, 9};
-        bubbleSort(arr);
-
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] bubbleSort(int[] arr){
+    public static void insertionSort(int[] arr){
         int n=arr.length;
-        for(int i=n-1;i>=1;i--) {
-            boolean swap=false;
-            for(int j=0;j<=i-1;j++) {
+        for(int i=1;i<n;i++) {
+            for(int j=i-1;j>=0;j--) {
                 if(arr[j] > arr[j+1]) {
                     int temp=arr[j+1];
                     arr[j+1]=arr[j];
                     arr[j]=temp;
-                    swap=true;
+                }
+                else {
+                    break;
                 }
             }
-            if(!swap) {
-                return arr;
-            }
         }
-        return arr;
     }
 }
